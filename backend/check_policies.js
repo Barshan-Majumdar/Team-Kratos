bc15bbc088547955aae7531bbcd3eff509591065
Barshan Majumdar <barshanmajumdar249@gmail.com>
2026-08-12 10:57:48 +0530

    Clean folder structure

--- backend/check_policies.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const user = await prisma.user.findFirst({where:{displayName: {contains: 'Soumyadip'}}});
-  console.log("User:", user.email, user.tenantId);
-  const policies = await prisma.leavePolicy.findMany({where:{tenantId: user.tenantId}});
-  console.log("Policies:", policies);
-}
-main().finally(() => prisma.$disconnect());
